a4549c7e45225faddd499e5ff9f8d63daad1fe46
t_tamada <tetsuhirotamada@gmail.com>
2014-03-13 13:24:53 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,22 +14,15 @@ tagged_train_sents = tagged_sents[:80]+tagged_sents[83:]
 tagged_gold_sents = tagged_sents[80:83]
 gold_sents = sents[80:83]
 tokenized_sents = file.get_tokenized_sents()
-"""for sent in tokenized_sents:
-    for token in sent:
-        print token+"  ","""
+
 
 print "--NUMBER OF SENTENCES--"
 print(len(tagged_sents))
 print "\n"
 
 print "--TOKENIZER--" 
-"""tokens = Tokenizer().tokenize(text)
-for token in tokens:
-    print "("+token+")",
-print "\n" """
 print "EVALUATE TOKENIZER"
 print "RATE"
-#Tokenizer().evaluate(tagged_gold_sents,gold_sents)
 Tokenizer().evaluate(tagged_sents,sents)
 print "ANSWER"
 gold_tokens = []
@@ -52,18 +45,14 @@ difference = set(test_tokens)-set(gold_tokens)
 for token in difference:
     print token+"  ",
 print "\n\nONLY IN ANSWER"
-difference2 = set(gold_tokens)-set(test_tokens)
-for token in difference2:
+difference = set(gold_tokens)-set(test_tokens)
+for token in difference:
     print token+"  ",
 print "\n"
 
 
 print "--TAGGER--"
 tagger = Tagger(tagged_train_sents)
-"""tagged_test_tokens = tagger.tag(Tokenizer().tokenize(text))
-for tup in tagged_test_tokens:
-    print str(tup[0])+"/"+str(tup[1]),"""
-print "\n"
 print "EVALUATE TAGGER"
 print "RATE"
 tagger.evaluate(tagged_gold_sents)
@@ -87,10 +76,8 @@ print "\n\nONLY IN ANSWER"
 difference2 = set(tagged_gold_tokens)-set(tagged_test_tokens)
 for tup in difference2:
     print str(tup[0])+"/"+str(tup[1]),
+print "\n"
 
-"""for sent in tagged_sents:
-    for tup in sent:
-        print str(tup[0])+"/"+str(tup[1]),
-print "\n" """  
+
 
 
